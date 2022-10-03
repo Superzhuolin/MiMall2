aa386bcf497f3ce0988457de137e4f902a18da04
Superzhuolin <1910185040@qq.com>
2022-10-04 00:34:14 +0800

    app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="app">hello</div>
+  <div id="app">
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import jsonp from "jsonp";
 export default {
   name: "App",
   components: {
-    age:30,
+    HelloWorld,
+  },
+  data() {
+    return {
+      age: 30,
+      data: "",
+    };
+  },
+  // mounted(){
+  //   let url ='http://github.com/easy-mock/easy-mock'
+  //    axios.get(url).then(()=>{
+  //    });
+  // },
+  mounted() {
+    let url = "https://www.imooc.com/activity/servicetime";
+    // axios(url).then(() => {});
+     
   },
 };
 </script>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
